06850fb4add16bffe1efd49440db0846f89a75eb
Jarbob <malew@ucsd.edu>
2025-03-03 10:26:24 -0800

    Uploading all my previous classes here

---
+++ Class04/class04.R
@@ -0,0 +1,12 @@
+#' ---
+#' title: "Class 4: Intro to R"
+#' author: "Mason Lew (PID:A17533139)"
+#' date: "Jan 16th, 2025"
+#' ---
+
+x <- 1:50
+plot(x, sin(x))
+#make this nicer
+plot(x, sin(x), col="blue", type="l", lwd=3)
+
+
